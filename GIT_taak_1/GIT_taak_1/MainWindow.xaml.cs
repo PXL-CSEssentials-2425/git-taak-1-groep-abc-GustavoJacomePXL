9b1b4af95a63a894c9ace5b5169b0ff84f8bea7e
GustavoJacomePXL <Gustavo.BarbosaJacome@student.pxl.be>
2024-10-06 18:35:14 +0200

    KLEUR-05

--- GIT_taak_1/GIT_taak_1/MainWindow.xaml.cs
+++ GIT_taak_1/GIT_taak_1/MainWindow.xaml.cs
@@ -18,34 +18,45 @@ namespace GIT_taak_1
     {
 
         string redCode = "#FF0000";
+        string redInfo = "Rood is de kleur van warmte";
+
         string greenCode = "#008000";
+        string greenInfo = "Groen is de kleur van genezing";
+
         string yellowCode = "#FFFF00";
+        string yellowInfo = "Geel is de kleur van levenslust";
+
         string blueCode = "#0000FF";
+        string blueInfo = "Blauw is de kleur van intelligentie";
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        // Toon de kleurencode als er op een kleur geklikt wordt
+        // Toon de kleurencode en informatie als er op een kleur geklikt wordt
         private void redImage_Click(object sender, RoutedEventArgs e)
         {
             codeTextbox.Text = redCode;
+            infoTextbox.Text = redInfo;
         }
 
         private void greenImage_Click(object sender, RoutedEventArgs e)
         {
             codeTextbox.Text = greenCode;
+            infoTextbox.Text = greenInfo;
         }
 
         private void yellowImage_Click(object sender, RoutedEventArgs e)
         {
             codeTextbox.Text = yellowCode;
+            infoTextbox.Text = yellowInfo;
         }
 
         private void blueImage_Click(object sender, RoutedEventArgs e)
         {
             codeTextbox.Text = blueCode;
+            infoTextbox.Text = blueInfo;
         }
 
         // Verrander de achtergrondkleur naar de corresponderende kleur als de gebruiker er over zweeft met de muis
